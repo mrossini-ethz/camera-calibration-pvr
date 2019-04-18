69d43e20d4416143ad1d7169e60d876e1137e8be
Marco Rossini <marco.rossini@protonmail.ch>
2019-04-18 23:22:15 +0200

    Made the operator set the camera background image to the reference image.

--- operator.py
+++ operator.py
@@ -1,9 +1,34 @@
 import bpy
 
+from . import reference
+
+def get_or_create_camera():
+    cam_obj = bpy.context.scene.camera
+    if not cam_obj:
+        bpy.ops.object.camera_add()
+        cam_obj = bpy.context.object
+    cam = bpy.data.cameras[cam_obj.data.name]
+    return (cam_obj, cam)
+
 class CameraCalibrationPVROperator(bpy.types.Operator):
     bl_idname = "camera.camera_calibration_pvr"
     bl_label = "Solve Camera Calibration"
 
     def execute(self, context):
-        print("Hello World")
+        # Get the properties
+        props = bpy.context.scene.camera_calibration_pvr_properties
+
+        # Reference image
+        image_obj = props.image
+        if not image_obj:
+            self.report({'ERROR'}, "Set a reference image.")
+            return {'CANCELLED'}
+        image, width, height, size, offset_x, offset_y = reference.get_reference_image_data(image_obj)
+
+        # Get the active camera (or create one)
+        camera_obj, camera = get_or_create_camera()
+
+        # Set the background image for the active camera (if necessary)
+        reference.camera_apply_reference_image(camera, image)
+
         return {'FINISHED'}
--- reference.py
+++ reference.py
@@ -1,5 +1,44 @@
 import bpy
 
-def get_empty_image_data(obj):
+def get_reference_image_data(obj):
     # Return image, width, height, scale, x-offset, y-offset for the given emtpy object (of image type)
     return (obj.data, obj.data.size[0], obj.data.size[1], obj.empty_display_size, obj.empty_image_offset[0], obj.empty_image_offset[1])
+
+def camera_apply_reference_image(camera, image):
+    # Get the background images from the active camera
+    needs_setting = True
+    images = camera.background_images
+    bkg_img = None
+
+    # Check whether it needs setting
+    for img in images:
+        if img.image == image:
+            bkg_img = img
+            break
+
+    if not bkg_img:
+        # Add a background image to the camera
+        area = bpy.context.area.type
+        bpy.context.area.type = "VIEW_3D"
+        bpy.ops.view3d.background_image_add()
+        bpy.context.area.type = area
+
+        # Set the background to the reference image
+        images = camera.background_images
+        bkg_img = images[-1]
+        bkg_img.image = image
+
+    # Ensure correct settings
+    if bkg_img.alpha == 0.0:
+        bkg_img.alpha = 0.5
+    bkg_img.display_depth = "BACK"
+    bkg_img.frame_method = "CROP"
+    bkg_img.offset[0] = 0.0
+    bkg_img.offset[1] = 0.0
+    bkg_img.rotation = 0.0
+    bkg_img.scale = 1.0
+    bkg_img.use_flip_x = False
+    bkg_img.use_flip_y = False
+
+    # Show background images
+    camera.show_background_images = True
